4858bb45b58015d89e8771f12cea0d7bd572f7b5
igorrm23 <igormachadofire14@gmail.com>
2026-01-09 22:58:23 -0300

    add pokemon image

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+const nomePokemon = document.getElementById("pokemon-name");
+const idPokemon = document.getElementById("pokemon-id");
+const imagePokemon = document.getElementById("screen-bg");
 
 async function LoadingPokemons() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
@@ -6,12 +8,25 @@ async function LoadingPokemons() {
     return data.results;
 }
 
+
+
 async function nomeAleatorio() {
     const pokemons = await LoadingPokemons();
     const pokemonAleatorio = pokemons[Math.floor(Math.random() * pokemons.length)];
 
-    const nomePokemon = document.getElementById("pokemon-name");
+    const pokemonId = pokemonAleatorio.url.split("/").at(-2);
+
     nomePokemon.textContent = pokemonAleatorio.name;
+    idPokemon.textContent = `#${pokemonId}`;
+
+    const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+    imagePokemon.setAttribute("href", imgUrl);
+
+    console.log(
+        "Pokemon:", pokemonAleatorio.name,
+        "ID:", pokemonId,
+    );
 }
+
 
 nomeAleatorio();
